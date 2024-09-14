ce1c12ff2eae559a9f8ed1cf8373aa17b6e118a0
Lavesh Patil <123832157+lavesh00@users.noreply.github.com>
2024-09-14 14:55:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -69,6 +69,15 @@ let scroll;
                   formWrapper.style.display = 'none';
                   contentWrapper.classList.remove('blur-background'); // Remove blur effect from the background
                 }
+              }function hidecard(event) {
+                // Hide the form wrapper if the click is outside the form
+                if (!event.target.closest('.form')) {
+                  const formWrapper = document.querySelector('.Card-wrapper');
+                  const contentWrapper = document.getElementById('page-wrapper');
+            
+                  formWrapper.style.display = 'none';
+                  contentWrapper.classList.remove('blur-background'); // Remove blur effect from the background
+                }
               }
             
               function showSignUp() {
